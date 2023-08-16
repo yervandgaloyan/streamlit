ab168c9d71ea161f0e7c42094c0ad158f5b7a2fe
yervandgaloyan <79852193+yervandgaloyan@users.noreply.github.com>
2023-08-16 23:25:26 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Hi from streamlit inside docker")
+st.title("Hi Mher")
 st.write("looks like this works properly.")
 st.write("What happens if you change the file?")
